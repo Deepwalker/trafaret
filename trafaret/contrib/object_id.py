2587edd1d86f976542fd556c9505b33c7dc1e6f8
Yehor Nazarkin <nimnull@gmail.com>
2013-02-27 20:52:02 +0200

    Purged blank value support

--- trafaret/contrib/object_id.py
+++ trafaret/contrib/object_id.py
@@ -11,9 +11,6 @@ class MongoId(Trafaret):
     convertable = str_types + (ObjectId,)
     value_type = ObjectId
 
-    def __init__(self, allow_blank=False):
-        self.allow_blank = allow_blank
-
     def __repr__(self):
         return "<MongoId(blank)>" if self.allow_blank else "<MongoId>"
 
@@ -25,11 +22,7 @@ class MongoId(Trafaret):
 
     def check_and_return(self, value):
 
-        if value is None:
-            self._failure('value not provided')
-        elif isinstance(value, self.convertable):
+        if isinstance(value, self.convertable):
             return value
-        elif len(value) is 0 and self.allow_blank:
-            return value
-        else:
-            self._failure('value is not %s' % self.value_type.__name__)
+
+        self._failure('value is not %s' % self.value_type.__name__)
