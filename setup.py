3e737bc608561571f8c87b09b83258e2b7360f8c
Mikhail Krivushin <krivushinme@gmail.com>
2014-03-21 20:12:29 +0400

    Updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setupconf = dict(
     name='trafaret',
-    version='0.4.9',
+    version='0.5.0',
     license='BSD',
     url='https://github.com/Deepwalker/trafaret/',
     author='Barbuza, Deepwalker, nimnull',
