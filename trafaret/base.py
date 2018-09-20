00266aec3b0b3453ed59f34518efe415df8be30a
Yehor Nazarkin <nimnull@gmail.com>
2018-09-20 14:33:06 +0300

    Drop unnesessary ensure_trafaret call

--- trafaret/base.py
+++ trafaret/base.py
@@ -934,7 +934,7 @@ class Dict(Trafaret, DictAsyncMixin):
         self.keys = list(args)
         for key, trafaret in itertools.chain(trafarets.items(), keys.items()):
             key_ = Key(key) if isinstance(key, str_types) else key
-            key_.set_trafaret(ensure_trafaret(trafaret))
+            key_.set_trafaret(trafaret)
             self.keys.append(key_)
         # optimized version without runtime check for context arg
         self._keys = [with_context_caller(key) for key in self.keys]
